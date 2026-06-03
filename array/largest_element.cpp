764eda64abc3bb57ca1282940aaecfdc483b8960
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-03 15:14:41 +0530

    correction in logic

--- array/largest_element.cpp
+++ array/largest_element.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int main()
 {
 
-    int arr[] = {1, 3, 5, 2, 5, 9, 6,44};
+    int arr[] = {1, 3, 55, 2, 5, 91, 6,44};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int largest = 0;
+    int largest = arr[0];
     for (int i = 0; i < size-1; i++)
     {
-        if (arr[i]<arr[i+1]){
-            largest =arr[i+1];
+        if(largest<arr[i+1]){
+            largest=arr[i+1];
         }
     }
 
     cout<<"Element in array"<<endl;
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
