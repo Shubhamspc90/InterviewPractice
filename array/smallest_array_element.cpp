2cbfc4315bdd0b36098f3bd397416a788712f15d
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-05 10:12:36 +0530

    comment are added

--- array/smallest_array_element.cpp
+++ array/smallest_array_element.cpp
@@ -1,27 +1,81 @@
-//  smallest element in array
-
+   //  Second Largest (distinct) element 
+   // with climits
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int main()
-{
+int main() {
 
-    int arr[] = {11,9,4,8,2,6,1};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int smallest = arr[0];
-    for (int i = 0; i < size-1; i++)
-    {
-        if(smallest>arr[i+1]){
-            smallest=arr[i+1];
+    int arr[] = {2, 5, 9, 5, 3, 2};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+
+    for(int i = 0; i < n; i++) {
+
+        if(arr[i] < smallest) {
+
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if(arr[i] < secondSmallest &&
+                arr[i] != smallest) {
+
+            secondSmallest = arr[i];
         }
     }
 
-    cout<<"Element in array"<<endl;
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    cout << "Smallest = " << smallest << endl;
 
-    cout<<"\nSmallest element in Array is "<<smallest;
+    if(secondSmallest == INT_MAX)
+        cout << "No second smallest distinct element";
+    else
+        cout << "Second Smallest = " << secondSmallest;
+
     return 0;
 }
+
+
+// // wiyhout climits
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int arr[] = {2, 5, 9, 5, 3, 2};
+
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     // Find smallest
+//     int smallest = arr[0];
+
+//     for(int i = 1; i < n; i++) {
+//         if(arr[i] < smallest) {
+//             smallest = arr[i];
+//         }
+//     }
+
+//     // Find second smallest distinct
+//     int secondSmallest = -1;
+
+//     for(int i = 0; i < n; i++) {
+
+//         if(arr[i] != smallest) {
+
+//             if(secondSmallest == -1 || arr[i] < secondSmallest) {
+//                 secondSmallest = arr[i];
+//             }
+//         }
+//     }
+
+//     cout << "Smallest = " << smallest << endl;
+
+//     if(secondSmallest == -1)
+//         cout << "No second smallest distinct element";
+//     else
+//         cout << "Second Smallest = " << secondSmallest;
+
+//     return 0;
+// }
