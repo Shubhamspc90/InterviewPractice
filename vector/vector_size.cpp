3c0cd6ca8253f674009ce310dc2b315342929175
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-08 11:46:18 +0530

    vector size complete

---
+++ vector/vector_size.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+  
+    vector< int > vec={1,3,4,8,5,9};
+    cout<<"size of vector : "<< vec.size();
+
+
+    return 0;
+}
