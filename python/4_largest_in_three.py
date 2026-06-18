4ee235d3df9c1f1ff886bb6d8744f476306e33d4
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-18 11:44:37 +0530

    largest in three

---
+++ python/4_largest_in_three.py
@@ -0,0 +1,7 @@
+a=int (input("Enter 1st number "))
+b=int (input("Enter 2nd number "))
+c=int (input("Enter 3nd number "))
+print(f"a = {a} , b = {b} , c= {c} ")
+
+largest=max(a,b,c)
+print(f"largest is {largest}")
