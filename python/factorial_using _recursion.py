efa2865d30dfe0cd7f07ab9630d1c24c41a75bd5
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-18 12:38:25 +0530

    fctorial by recursion done

---
+++ python/factorial_using _recursion.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    return n*factorial(n-1)
+
+num=int(input("Enter a number\t"))   
+fact=factorial(num)
+print(f"factorial of {num} is {fact}")
