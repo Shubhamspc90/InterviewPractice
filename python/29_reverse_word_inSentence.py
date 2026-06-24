fa80b4b6d37be56e20eddd57bbd640db3126c7a3
Shubham Chauhan <shubham.spc90@gmail.com>
2026-06-24 11:07:16 +0530

    Reversing the word in sentence done

---
+++ python/29_reverse_word_inSentence.py
@@ -0,0 +1,11 @@
+
+#  # method 1 
+# sentence=input("Enter a sentence  ")
+# word = sentence.split()
+# word.reverse()
+# print(" ".join(word))
+
+# # method 2
+sentence = input("Entera sentence  ")
+word = sentence.split()
+print(" ".join(word[::-1]))
