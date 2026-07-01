f1d0df03c172c1144f5569fcd21d9cdeeab07bef
Shubham Chauhan <shubham.spc90@gmail.com>
2026-07-01 12:27:02 +0530

    added 2nd method

--- python/33_second_largest.py
+++ python/33_second_largest.py
@@ -7,3 +7,11 @@ print(f"Original list: {my_list}")
 result=sorted(my_list)
 print(f"after sorting {result}")
 print(f"Second largest: {result[-2]}")
+
+print(end="\n")
+
+l1=[3,2,6,1,5,9,7,4]
+print(f"Original list: {l1}")
+l1.sort()
+print(f"after sorting {l1}")
+print(f" Second largest element: {l1[-2]}")
