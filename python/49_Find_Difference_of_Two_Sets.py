38d81960330c0e05e370639f2261dab82d842cbe
Shubham Chauhan <shubham.spc90@gmail.com>
2026-07-18 01:48:54 +0530

    Difference of Two Sets done

---
+++ python/49_Find_Difference_of_Two_Sets.py
@@ -0,0 +1,16 @@
+# Find Difference of Two Sets
+
+s1 = {1,2,3,4}
+s2 = {3,4,5,6}
+
+print("s1: ",s1)
+print("s2:",s2)
+result1 = s1 - s2
+result2 = s2-s1
+print("Difference of Two Sets s1-s2")
+print(result1)
+
+print("Difference of Two Sets s2-s1")
+print(result2)
+
+
